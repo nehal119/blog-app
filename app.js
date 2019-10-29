b24de21ff1daa3d993b9412bd835064e7e23fc55
nehal119 <34517705+nehal119@users.noreply.github.com>
2019-10-29 23:12:15 +0530

    previews added

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = "mongodb://localhost:27017/blog_app";
-mongoose.connect(uri, { useNewUrlParser: true, });
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
